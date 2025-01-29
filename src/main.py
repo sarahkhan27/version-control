5e46e27274c290c4c7d06e056a87491690c6aeb9
khans5 <sk@class-docker>
2025-01-29 09:34:14 +0000

    adjusting timezones

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 # Acknowledging the help of some  website tools for fixing code errors
 
 from datetime import datetime
+from zoneinfo import ZoneInfo #to adjust the VSCode timezone to PST
 
 # Current date and time in required formatS
-current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+current_time = datetime.now(ZoneInfo("US/Pacific")).strftime("%Y-%m-%d %H:%M:%S")
 print(f"Current date and time: {current_time}")
 
 # Writing to version.md
 with open("../version.md", "w") as file:
-    file.write(f"Current date and time: {current_time}")
+    file.write(f"Current date and time (PST): {current_time}")
