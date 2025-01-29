e6f4a2d628c6037927b085190ea8bb4f65bdf700
khans5 <sk@class-docker>
2025-01-29 09:05:55 +0000

    updating adding

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
-# Acknowledging the use of websites/ AI tools for additional help with date and time code
-import datetime
+# Acknowledging the use of website tools for additional help
 
-# Ccurrent date and time in correct format YYYY-MM-DD HH:MM:SS
-current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+from datetime import datetime
 
-# Writing  date/time to version.md
-with open("../../version.md", "w") as file:
+# Current date and time in required formatS
+current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+# Writing to version.md
+with open("version.md", "w") as file:
     file.write(f"Current Date and Time: {current_time}")
 
