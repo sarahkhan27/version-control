ad3520c3a8248a70cb9bd2c96da0a816f4b33549
khans5 <sk@class-docker>
2025-01-29 10:00:03 +0000

    timezone adjustments

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 # Acknowledging referance to/ help from some website tools for fixing codes/ errors
 
 from datetime import datetime
+import pytz
 
-# Current date and time in required format
-current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-print(f"Current date and time: {current_time}") 
+#PST timezone 
+timezone = pytz.timezone("US/Pacific")
+
+# Current date and time in required format PST
+current_time = datetime.now(timezone).strftime("%Y-%m-%d %H:%M:%S")
+print(f"Current date and time: {current_time}")
 
 # Write the current date and time to version.md
-with open("version.md", "w") as file:
-    file.write(f"Current date and time: {current_time}")
+with open("../version.md", "w") as file:
+    file.write(f"Current date and time (PST): {current_time}")
 
